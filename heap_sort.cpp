712463e3641862c453b1cf5e37e3db32d03708b2
Raj Kumar Tiwari <77829878+rktiwari360@users.noreply.github.com>
2022-12-17 21:07:29 +0530

    Add files via upload

---
+++ heap_sort.cpp
@@ -0,0 +1,49 @@
+// nlog(n).........
+#include <iostream>
+using namespace std;
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    if (l < n && arr[largest] < arr[l])
+        largest = l;
+    if (r < n && arr[largest] < arr[r])
+        largest = r;
+
+    if (largest != i)
+    {
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, largest);
+    }
+}
+void build(int arr[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+}
+void heap(int arr[], int n)
+{
+    build(arr, n);
+    for (int i = n - 1; i >= 0; i--)
+    {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+int main()
+{
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    heap(arr, n);
+    printArray(arr, n);
+}
